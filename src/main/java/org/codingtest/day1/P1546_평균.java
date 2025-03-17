4d578a4571d2336081c72042ce1bb388fce72fe6
Jinny <jhs7251@naver.com>
2025-03-17 17:29:04 +0900

    CodingTest day2 commit

---
+++ src/main/java/org/codingtest/day1/P1546_평균.java
@@ -0,0 +1,23 @@
+package org.codingtest.day1;
+
+import java.util.Scanner;
+
+public class P1546_평균 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int A[] = new int[N];
+        for (int i=0;i<N;i++){
+            A[i] = sc.nextInt();
+        }
+        long sum = 0;
+        long max = 0;
+        for (int i=0;i<N;i++){
+            sum += A[i];
+            if (A[i] > max){
+                max = A[i];
+            }
+        }
+        System.out.println(sum * 100.0 / max / N);
+    }
+}
